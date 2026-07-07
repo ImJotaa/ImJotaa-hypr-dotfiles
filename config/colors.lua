0b928c49859ff1dcb84d8a91d490bd841e74918b
imjotaa <jonaslab18@gmail.com>
2026-07-07 21:11:19 +0200

    Merge repo with local files.

---
+++ config/colors.lua
@@ -0,0 +1,23 @@
+-- Neon Cyberpunk palette
+
+return {
+
+	-- ------- NEON COLORS ------- -- 
+	magenta = "#ff2fd4", -- Magenta
+	cyan = "#00f0ff", -- Cyan
+	violet = "#a239ff", -- Violet
+	green = "#39ff8c", -- Green
+	yellow = "#fcee0a", -- Yellow
+	orange = "#ff5f1f", -- Orange
+	blue = "#2f6fff", -- Blue
+	red = "#ff073a", -- Red
+
+	-- ------- MONOCHROME ------- --
+	black = "#0a0612", -- Background 
+	dark = "#140b26", -- Elevated surface
+	dark_alt = "#1c1230", -- Border / Panels
+	gray_dark = "#2a1c47", -- Light borders / separators
+	gray = "#8b84ab", -- Muted
+	gray_light = "#c7c2dc", -- Atenuated
+	white = "#e9e6f5", -- Main text
+}
